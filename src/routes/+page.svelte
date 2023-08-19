a532265a5d083cbd0e8be08605cfad92758fa066
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-19 08:13:37 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
     let a = 0
 </script>
 
+<h1>Cica</h1>
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <img id="cica" on:click={() => a++} src={cica} alt="">
 <br>
-{a}
+<div id="x">{a}</div>
